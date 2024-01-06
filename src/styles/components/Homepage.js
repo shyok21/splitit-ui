6dcd1bc635007dc2ee7480abf606b2debd6802ff
Shyok Mutsuddi <shyokmutsuddi21@gmail.com>
2024-01-07 01:05:11 +0530

    Homepage: AppBar Added

---
+++ src/styles/components/Homepage.js
@@ -0,0 +1,32 @@
+import { Constant } from "../../utils/constant";
+import styled from "styled-components";
+
+const HomepageBody = styled.div`
+    width: 100vw;
+    height: 100vh;
+    overflow: auto;
+    background-color: ${Constant.defaultBackgroundColor};
+    color: ${Constant.defaultTextColor};
+    display: flex;
+    flex-direction: column;
+`
+
+const appBarStyle = {
+    width: '100%',
+    height: '70px',
+    backgroundColor: Constant.darkBackgroundColor,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
+}
+
+const HomepageHeading3 = styled.h3`
+    margin: 12px;
+    width: 80%;
+`
+
+export {
+    HomepageBody,
+    HomepageHeading3,
+    appBarStyle
+}
---
+++ src/utils/constant.js
@@ -0,0 +1,12 @@
+const Constant = {
+    'darkBackgroundColor': '#151554',
+    'defaultBackgroundColor': '#191970',
+    'lightBackgroundColor': '#282896',
+    'defaultTextColor': '#E6E6FA',
+    'greenTextColor': '#98FF98',
+    'redTextColor': '#F08080',
+    'owedDefaultText': 'Funds coming your way!',
+    'owingDefaultText': 'Cash me outside!'
+}
+
+export { Constant }
